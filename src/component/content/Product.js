8d0f23cac70b3ff1d6eac1df7d61945d305f04e1
husenzaini <husenhisamitsu@gmail.com>
2020-02-17 21:31:55 +0700

    get data

--- src/component/content/Product.js
+++ src/component/content/Product.js
@@ -1,6 +1,16 @@
 import React, {Component} from 'react';
 import './Product.css'
+import axios from 'axios'
 class Product extends Component{
+    componentDidMount(){
+        axios.get('http://localhost:4002/api/v1/product')
+        .then(res => {
+            console.log(res)
+      })
+        .catch(err => {
+          console.log(err)
+      })      
+    }
     render(){
         return(
             <div className='main'>
